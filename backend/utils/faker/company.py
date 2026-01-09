3beb9697e36ccefea5d1bc5b71a74f3fdf80178b
ua16 <ua16@users.noreply.github.com>
2026-01-09 11:05:34 +0530

    Extended faker submodule

--- backend/utils/faker/company.py
+++ backend/utils/faker/company.py
@@ -1,7 +1,18 @@
 from .names.last_names import last_names
+from .names.locations import locations
+from .names.roads import roads
 from random import choice
+from random import randint
 
 com_ends = ["LLC", "Inc", "Corp", "Pvt Ltd"]
+email_providers = ["supermail.com", "kmail.net", "mail.com", "letters.net"]
 
-def name():
-    return f"{choice(last_names)} {choice(com_ends)}"
+class Company:
+    def __init__(self) -> None:
+        self.uniq_name = {choice(last_names)}
+        self.com_end = {choice(com_ends)}
+        self.name = f"{self.uniq_name} {self.com_end}"
+        self.email = f"{self.uniq_name}{self.com_end}@{choice(email_providers)}"
+        self.city = choice(locations)
+        self.road = choice(roads)
+        self.address = f"{randint(1,500)}, {self.road} , {self.city}"
